1b53ffa71ff6a6e2d909e88ae3f324004d4c016d
Himanshu Negi <51152254+Bot-7037@users.noreply.github.com>
2021-07-20 11:43:36 +0530

    Update 01_HelloWorld.scala

--- 01_HelloWorld.scala
+++ 01_HelloWorld.scala
@@ -1,5 +1,6 @@
+// Hello world program
 object HelloWorld {
-  def main(args: Array[String]): Unit = {
+  def main(args: Array[String]): Unit = { // Unit represents no return type
     println("Hello World!")
   }
 }

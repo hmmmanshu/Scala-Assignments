57fd16e0da3fbfd0aa902134b830992578a78186
Himanshu Negi <51152254+Bot-7037@users.noreply.github.com>
2021-07-20 11:50:09 +0530

    Create 03_RelationalOperators.scala

---
+++ 03_RelationalOperators.scala
@@ -0,0 +1,16 @@
+//Demonstrate  the Relational Operators
+object RelationalOperators {
+
+  def main(args: Array[String]) {
+    var a = 50;
+    var b = 30;
+
+    println("a == b is : " + (a == b));
+    println("a != b is : " + (a != b));
+    println("a > b is : " + (a > b));
+    println("a < b is : " + (a < b));
+    println("a >= b is : " + (a >= b));
+    println("a <= b is : " + (a <= b));
+
+  }
+}

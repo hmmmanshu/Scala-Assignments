488cbaf59e4d7d95623a827fc8ee85c15ea5ea4b
Himanshu Negi <51152254+Bot-7037@users.noreply.github.com>
2021-07-20 11:46:48 +0530

    Create 02_AirthmeticOperators.scala

---
+++ 02_AirthmeticOperators.scala
@@ -0,0 +1,16 @@
+// Demonstrate the Airthmetic Operators
+
+object AirthmeticOperators {
+
+  def main(args: Array[String]) {
+    var a = 50;
+    var b = 30;
+
+    println("a + b = " + (a + b));
+    println("a - b = " + (a - b));
+    println("a * b = " + (a * b));
+    println("a / b = " + (a / b));
+    println("a % b = " + (a % b));
+
+  }
+}

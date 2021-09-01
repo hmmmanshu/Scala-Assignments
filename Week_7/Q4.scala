1b2fe573bf94e80e59892c3d4cedc10d8e825f31
Himanshu Negi <51152254+Bot-7037@users.noreply.github.com>
2021-09-01 23:25:11 +0530

    Update Q4.scala

--- Week_7/Q4.scala
+++ Week_7/Q4.scala
@@ -1,10 +1,28 @@
-object Q4{
-    def removeDuplicates(list: List[int]){
-        var newList = list.distinct;
-        println(newList);
-    }
-    
-    def main(args: Array[String]){
-        
-    }
+object Q4 {
+  def removeDuplicates(list: List[Int]) {
+    var newList = list.distinct;
+    println(newList);
+  }
+  def find(list: List[Int], ele: Int, ind: Int) {
+    return list.find(ele, ind);
+  }
+  def listLength(list: List[Int]) {
+    return list.length;
+  }
+  def sort(list: List[Int]) {
+    return list.sorted;
+  }
+  def sum(list: List[Int]) {
+    return list.sum;
+  }
+  def listToString(list: List[Int]) {
+    return list.toString();
+  }
+  def smallest(list: List[Int]) {
+    return list.min;
+  }
+  def maximum(list: List[Int]) {
+    return list.max;
+  }
+  def main(args: Array[String]) {}
 }

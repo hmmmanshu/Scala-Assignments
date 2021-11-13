8d0a0ec8a37260866536a1641ba5958e4e848c89
Himanshu Negi <51152254+Bot-7037@users.noreply.github.com>
2021-11-13 07:54:40 +0530

    Update 08.scala

--- End_term_Assignment/08.scala
+++ End_term_Assignment/08.scala
@@ -3,8 +3,9 @@ object Q2{
         var name = "Himanshu";
         print(isEligible(name));
     }
+    var valid_age = 18;
     def isEligible(age: Int, name: String) => {
-        if(age>=18)
+        if(age>=valid_age)
             println(s"$name of age $age is eligible");
         else
             println(s"$name of age $age is ineligible");

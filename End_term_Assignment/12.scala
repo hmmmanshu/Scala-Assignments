952d84af2fee075fb249ade2d4eda3ba036b95c6
Bot-7037 <hnegi12345@gmail.com>
2021-11-13 21:35:16 +0530

    Add Data Science methods

--- End_term_Assignment/12.scala
+++ End_term_Assignment/12.scala
@@ -1,17 +1,18 @@
-object dataprep{
-    def dataprep(data: DataFrame): DataFrame = {
-        val data_prep = data.withColumn("label", data("label").cast(DoubleType))
-        val data_prep_indexed = data_prep.withColumn("label", data_prep("label").cast(DoubleType))
-        val data_prep_indexed_norm = data_prep_indexed.withColumn("label", data_prep_indexed("label").cast(DoubleType))
-        val data_prep_indexed_norm_indexed = data_prep_indexed_norm.withColumn("label", data_prep_indexed_norm("label").cast(DoubleType))
-        val data_prep_indexed_norm_indexed_scaled = data_prep_indexed_norm_indexed.withColumn("label", data_prep_indexed_norm_indexed("label").cast(DoubleType))
-        data_prep_indexed_norm_indexed_scaled
+import org.ddahl.rscala._
+import ru.retailrocket.ispark._
+object DataScience{
+    def connect = RClient("R", false)
+    @transient
+    val r = connect()
+    def dataViz():Unit={
+        R.plot(r.c("x", 1 to 10), r.c("y", 1 to 10)) // Random data to visualize
     }
     def dataDescription(data: DataFrame): Unit = {
-        val data_desc = data.describe()
+        val data_desc = data.describe() // Summarize the data usnig inbuilt functions
         data_desc.show()
     }
     def main(args: Array[String]): Unit = {
-        
+        dataViz()
+        dataDescription("himanshu/Desktop/sample.csv")
     }
 }
